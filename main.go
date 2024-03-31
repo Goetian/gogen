883b782b9b27af5b028d683b65d477a85bff1cae
Goetian <gitEmail>
2024-04-01 01:21:16 +0200

    installed all depen, base setup

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"embed"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/goetian/gogen/handler"
 	"github.com/joho/godotenv"
 )
+
+//go:embed public
+var FS embed.FS
 
 func main() {
 
@@ -17,6 +22,9 @@ func main() {
 		log.Fatal(err)
 	}
 	router := chi.NewMux()
+	router.Handle("/*", http.StripPrefix("/", http.FileServer(http.FS(FS))))
+	router.Get("/", handler.MakeHandler(handler.HandleHomeIndex))
+
 	port := os.Getenv("HTTP_LISTEN_ADDR")
 	slog.Info("Server Up", "port", port)
 	log.Fatal(http.ListenAndServe(port, router))
